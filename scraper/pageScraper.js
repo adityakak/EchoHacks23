674a5f509d9a728160a5bc11455f07001e7e81f7
akashpamal <akashpamal@gmail.com>
2023-04-15 13:09:12 -0400

    trying to scrape amazon

--- scraper/pageScraper.js
+++ scraper/pageScraper.js
@@ -27,19 +27,21 @@ const scraperObject2 = {
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url);
         // Wait for the required DOM to be rendered
-        await page.waitForSelector('.a-page');
+        await page.waitForSelector('.a-section');
+        console.log("Page render complete")
         // Get the link to all the required books
         let urls = await page.$$eval('section ol > li', links => {
             // Make sure the book to be scraped is in stock
-            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+            links = links.filter(link => link.querySelector('#productTitle').textContent !== "")
             // Extract the links from the data
             links = links.map(el => el.querySelector('h3 > a').href)
             return links;
         });
+        console.log("Logging URLS")
         console.log(urls);
 
     }
 }
 
-module.exports = scraperObject1;
-// module.exports = scraperObject2;
+// module.exports = scraperObject1;
+module.exports = scraperObject2;
